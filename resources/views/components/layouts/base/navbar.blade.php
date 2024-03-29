2fa64337a886549feb840f4a097efdb727fb34fb
aryamaahendra <arya.mahendra30k@gmail.com>
2024-03-29 12:33:25 +0800

    add: layout base

---
+++ resources/views/components/layouts/base/navbar.blade.php
@@ -0,0 +1,46 @@
+<div class="shadow-md">
+    <div class="w-full max-w-6xl mx-auto navbar bg-base-100">
+        <div class="navbar-start">
+            <div class="dropdown">
+                <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none"
+                        viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h8m-8 6h16" />
+                    </svg>
+                </div>
+                <ul tabindex="0"
+                    class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><a>Item 1</a></li>
+                    <li>
+                        <a>Parent</a>
+                        <ul class="p-2">
+                            <li><a>Submenu 1</a></li>
+                            <li><a>Submenu 2</a></li>
+                        </ul>
+                    </li>
+                    <li><a>Item 3</a></li>
+                </ul>
+            </div>
+            <a class="text-xl btn btn-ghost">daisyUI</a>
+        </div>
+        <div class="hidden navbar-center lg:flex">
+            <ul class="px-1 menu menu-horizontal">
+                <li><a>Item 1</a></li>
+                <li>
+                    <details>
+                        <summary>Parent</summary>
+                        <ul class="p-2">
+                            <li><a>Submenu 1</a></li>
+                            <li><a>Submenu 2</a></li>
+                        </ul>
+                    </details>
+                </li>
+                <li><a>Item 3</a></li>
+            </ul>
+        </div>
+        <div class="navbar-end">
+            <a href="{{ route('login') }}" class="btn btn-primary">Login</a>
+        </div>
+    </div>
+</div>
